9a1fb42a3a7c0635497d048a5d7a0818313396e9
Carl Girouard <cgiroua@us.ibm.com>
2019-01-17 17:53:29 -0500

    Need to have jq to dump the keys, and removing the apt specific packages

--- cloud/edge-hardening.sh
+++ cloud/edge-hardening.sh
@@ -9,14 +9,12 @@ exec 2> >(tee -a ${LOG} >&2)
 
 set -x
 
-date
-
-#### Workaround for ssh-keys authorize_keys while -k doesn't work with cloud CLI
-mkdir -m 700 /root/.ssh
-curl https://api.service.softlayer.com/rest/v3/SoftLayer_Resource_Metadata/getUserMetadata.json 2>/dev/null | tr -d \'\\ 2>/dev/null | tail -c +2 | head -c -1 | jq -M -r '.["cg-key"], .["my-key"]' | sort -u > /root/.ssh/authorized_keys
-
 # keep lots of system logs
 apt-get update
+
+# Something is colliding with this script during startup, so let's wait it out
+sleep 60
+
 #### On the fence about purging this, or enabling it and adding auto kernel cleanup ... Purge is better for stable
 #### dev environments, enabled is obviously better for security, since this is a hardening script, let's keep it
 #apt-get purge -y ufw unattended-upgrades
@@ -24,12 +22,11 @@ apt-get update
 sed -i 's/^\/\/Unattended-Upgrade.*/Unattended-Upgrade::Remove-Unused-Kernel-Packages "true";/' /etc/apt/apt.conf.d/50unattended-upgrades
 systemctl restart unattended-upgrades
 
-date
+apt-get install -y vim htop iftop tree auditd tcpdump bsd-mailx ssmtp jq logwatch ntp rsyslog
 
-apt-get install -y cron-apt aptitude vim htop iftop tree auditd tcpdump bsd-mailx ssmtp jq logwatch ntp rsyslog
-# Hacky, and probably still won't work, we're bumping into something terminating the script here during the package installs
-dpkg --configure -a
-apt-get install -y cron-apt aptitude vim htop iftop tree auditd tcpdump bsd-mailx ssmtp jq logwatch ntp rsyslog
+#### Workaround for ssh-keys authorize_keys while -k doesn't work with cloud CLI
+mkdir -m 700 /root/.ssh
+curl https://api.service.softlayer.com/rest/v3/SoftLayer_Resource_Metadata/getUserMetadata.json 2>/dev/null | tr -d \'\\ 2>/dev/null | tail -c +2 | head -c -1 | jq -M -r '.["cg-key"], .["my-key"]' | sort -u > /root/.ssh/authorized_keys
 
 # configure ssh access
 usermod -aG ssh root
