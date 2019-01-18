e8fb42ec5235f2c5a1d889e6f8ccb33b9213ca52
Carl Girouard <cgiroua@us.ibm.com>
2019-01-17 20:15:19 -0500

    Changing how we install docker

--- cloud/edge-hardening.sh
+++ cloud/edge-hardening.sh
@@ -56,8 +56,9 @@ EOF
 /etc/network/if-pre-up.d/iptables
 
 # install docker
-curl https://get.docker.com/ | sh
-systemctl enable docker && systemctl restart docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get install -y docker-ce
 
 # Keep more syslogs
 sed -i 's/rotate.*/rotate 20/' /etc/logrotate.d/rsyslog
